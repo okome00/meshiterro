462775bcc24a469b5be10e7740cbe035275b2ae9
EC2 Default User <ec2-user@ip-172-31-33-93.ec2.internal>
2022-03-18 15:26:59 +0000

    [Update]アプリケーションを完成させよう2：17章

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,12 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   # 画像が投稿されていない場合のエラー回避（画像サイズの変換処理）
-  def get_profile_image
+  def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', oontent_type: 'image/jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.varian(resize_to_limit: [width, height]).processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   # ルートパス（urlの一番最初 .com時のぺージ） => aboutページ
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
-  # Userモデル(devise：ユーザー認証機能)のルーディング 
+  # Userモデル(devise：ユーザー認証機能)のルーディング
   devise_for :users
-  
+
   # PostImageモデルのルーディング
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   # usersコントローラーのルーディング
-  resources :users, onle: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 
 end
